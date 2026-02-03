b7d342ee7e3be8a65b9bd698305c465f20b00f5c
Sam Kiessler <sam@kiessler.co.uk>
2026-02-03 23:19:10 +0000

    more tests :o

---
+++ internal/azure/resources/resources_test.go
@@ -0,0 +1,72 @@
+package azure
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/assert"
+	"github.com/varantha/az-jit/internal/clients"
+)
+
+type credentialMock struct{}
+
+func (c credentialMock) GetToken(ctx context.Context, req policy.TokenRequestOptions) (azcore.AccessToken, error) {
+	return azcore.AccessToken{
+		Token: "fake-token",
+	}, nil
+}
+
+func Test_ListEligibleAssignments(t *testing.T) {
+
+}
+
+func Test_GetRoleDefinitionName(t *testing.T) {
+
+}
+
+func Test_GetActiveLinkedRoleAssignmentIDs(t *testing.T) {
+	defer gock.Off()
+	gock.DisableNetworking()
+	defer gock.EnableNetworking()
+
+	route := "/providers/Microsoft.Authorization/roleAssignmentScheduleInstances"
+	response, err := os.ReadFile("testdata/activeassignments.json")
+	if err != nil {
+		t.Fatalf("failed to read test data file: %v", err)
+	}
+
+	gock.New("https://management.azure.com").
+		Get(route).
+		Reply(200).
+		BodyString(string(response))
+
+	hc := &http.Client{}
+	gock.InterceptClient(hc)
+
+	armOptions := &arm.ClientOptions{
+		ClientOptions: policy.ClientOptions{
+			Transport: hc,
+		},
+	}
+
+	OCOptions := clients.OmniClientOptions{
+		ARM: armOptions,
+	}
+
+	cred := &credentialMock{}
+	client, err := clients.NewOmniClient(cred, OCOptions)
+
+	activeIDs, err := GetActiveLinkedRoleAssignmentIDs(context.Background(), client)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	expectedCount := 1
+	assert.Equal(t, expectedCount, len(activeIDs))
+}
